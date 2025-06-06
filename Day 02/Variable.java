f73f27957e036a7761eda4b72623da69518a3206
AnujPratap-96 <offcialthakur94@gmail.com>
2025-06-06 08:20:31 +0530

    "Day 02 Variables Done"

---
+++ Day 02/Variable.java
@@ -0,0 +1,22 @@
+class Variable {
+public static void main(String[] args) {
+    // Declare a variable
+    int myNumber = 10;
+
+    // Print the variable
+    System.out.println("The value of myNumber is: " + myNumber);
+
+    // Change the value of the variable
+    myNumber = 20;
+
+    byte by = 127;
+    short sh = 558;
+    long lo = 123456789L;
+    float fl = 3.14f;
+    double db = 3.14159;
+    char ch = 'A';
+    boolean isTrue = true;
+    // Print the new value of the variable
+    System.out.println("The new value of myNumber is: " + myNumber);
+  }
+}
